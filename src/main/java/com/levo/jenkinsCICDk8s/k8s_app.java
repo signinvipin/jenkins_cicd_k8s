23d82bed7b615b3077bad0376ab3d23fc332c652
signinvipin <signinvipin@gmail.com>
2025-09-25 15:07:49 +0530

    Add change to file name

--- src/main/java/com/levo/jenkinsCICDk8s/k8s_app.java
+++ src/main/java/com/levo/jenkinsCICDk8s/k8s_app.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DockerApp {
+public class k8s_App {
 	public static void main(String[] args) {
-		SpringApplication.run(DockerApp.class, args);
+		SpringApplication.run(k8s_App.class, args);
 	}
 }
